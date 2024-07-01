03809a7e7386541027826e5727e23aa2e60bba16
JBC <hello@jeanbaptistecalzia.fr>
2024-07-01 13:48:02 +0200

    Add script exercice3

--- Exercice3/index.js
+++ Exercice3/index.js
@@ -1,8 +1,18 @@
 // Créez votre fonction ici
+function calculateAverage(arrayOfNumbers = []) {
+  let numbers = arrayOfNumbers;
 
+  if (numbers.length == 0) {
+    console.log("No numbers to calculate average");
+    return false;
+  } else {
+    const average = numbers.reduce((a, b) => a + b, 0) / numbers.length;
+    return average;
+  }
+}
 // Exemples d'utilisation de la fonction
-console.log(calculateAverage([5, 10, 15])) // retourne 10
-console.log(calculateAverage([10, 20, 30, 20])) // retourne 20
-console.log(calculateAverage()) // No numbers to calculate average
+console.log(calculateAverage([5, 10, 15])); // retourne 10
+console.log(calculateAverage([10, 20, 30, 20])); // retourne 20
+console.log(calculateAverage()); // No numbers to calculate average
 
-export default calculateAverage
+export default calculateAverage;
