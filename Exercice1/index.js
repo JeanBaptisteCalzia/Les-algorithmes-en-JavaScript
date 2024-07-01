73c82da425e32d96065fecb242f2c1b5453ce425
JBC <hello@jeanbaptistecalzia.fr>
2024-07-01 13:39:28 +0200

    Add script exercice 1

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,24 @@
 //votre code ici
+function pairNumbers(a, b) {
+  // Initializing numbers array
+  let numbers = [];
+  // Declaring empty evenNumbers array
+  let evenNumbers = [];
 
-export default pairNumbers
+  for (let i = a; i < b + 1; i++) {
+    numbers.push(i);
+  }
+
+  numbers.forEach((element) => {
+    if (element % 2 == 0) {
+      evenNumbers.push(element);
+    }
+  });
+
+  return evenNumbers.toString();
+}
+
+const displayNumber = pairNumbers(1, 10);
+console.log(displayNumber);
+
+export default pairNumbers;
